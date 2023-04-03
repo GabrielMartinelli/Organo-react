88f66b81bc5e81a9bbc9a828bcacbf1fb0fa7459
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-04-03 15:50:54 -0300

    adicionada função remover colaborador e adicionar time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,20 +1,24 @@
 import Colaborador from "../Colaborador";
+import hexToRgba from "hex-to-rgba";
 import "./time.css";
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
   return (
     colaboradores.length > 0 && (
-      <section className="time" style={{ backgroundImage: "url(/img/fundo.png)", backgroundColor: time.corPrimaria }}>
+      <section
+        className="time"
+        style={{ backgroundImage: "url(/img/fundo.png)", backgroundColor: hexToRgba(time.cor, "0.6") }}
+      >
         <input
-          onChange={(evento) => mudarCor(evento.target.value, time.nome)}
-          value={time.corSecundaria}
+          onChange={(evento) => mudarCor(evento.target.value, time.id)}
+          value={time.cor}
           type="color"
           className="input-cor"
         />
-        <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
         <div className="colaboradores">
           {colaboradores.map((colaborador, indice) => (
-            <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
+            <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />
           ))}
         </div>
       </section>
